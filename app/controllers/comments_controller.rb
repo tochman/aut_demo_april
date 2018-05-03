39b19eb53200c5b480b681ff77b6712d4fed8e81
faraznaeem <faraznaeem@gmail.com>
2018-05-03 14:36:00 +0200

    Sanitize email params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
 
   private
   def comments_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :email)
   end
 
 end
